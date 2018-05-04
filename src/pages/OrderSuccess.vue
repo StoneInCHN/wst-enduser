18be11f07083ebdb676ccff9a354d1ee379e8244
tanbiao <tankboo@163.com>
2018-05-04 21:26:47 +0800

    美化订单成功页面

--- src/pages/OrderSuccess.vue
+++ src/pages/OrderSuccess.vue
@@ -1,16 +1,23 @@
 <template>
   <div class="app-order-success">
-      <p>购买成功</p>
+      <p>
+        <Icon class="icon-success" name="certificate"/>
+      </p>
+      <p class="msg">购买成功</p>
       <h4>您购买的商品马上送到</h4>
       <p>货到后，你还需要支付 <span>￥{{successOrder.cobAmount}}</span></p>
-      <p>您可以再次扫描二维码关注订单状态</p>
+      <p class="desc">您可以再次扫描二维码关注订单状态</p>
   </div>
 </template>
 <script>
+import { Icon } from "vant";
 import { mapActions, mapGetters } from "vuex";
 export default {
   name: "OrderSuccess",
-  computed:{
+  components: {
+    Icon
+  },
+  computed: {
     ...mapGetters(["successOrder"])
   }
 };
@@ -23,13 +30,24 @@ export default {
   height: 100%;
   box-sizing: border-box;
   h4 {
-    margin: 40px 0 20px 0;
+    margin: 10px;
   }
   span {
     color: red;
   }
   p:last-child {
     margin-top: 50px;
+  }
+  .icon-success {
+    font-size: 100px;
+    color: #00a0e9;
+  }
+  .msg{
+    color: #00a0e9;
+  }
+  .desc{
+    font-size: 14px;
+    color: #666;
   }
 }
 </style>
