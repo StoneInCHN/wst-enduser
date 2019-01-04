5f9ddf4d5f632cdede29ab8e57d6a972430bc0bb
tanbiao <tankboo@163.com>
2019-01-04 22:54:04 +0800

    fix

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,4 +1,4 @@
 module.exports = {
   NODE_ENV: '"production"',
-  BASE_URL:'"http://www.yunshuizhan.cn/wst-customer"' //部署生产环境时候用这个
+  BASE_URL:'"http://www.yunshuizhan.cn:20001/wst-customer"' //部署生产环境时候用这个
 }
